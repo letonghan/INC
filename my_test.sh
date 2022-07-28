c68386995be4e4cc3443fcc6574f7d4569e1a3a5
Han, Letong <letong.han@intel.com>
2022-07-28 14:21:33 +0800

    add azure test bash file

---
+++ my_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+function main {
+  echo "Installing requirements"
+  pip install -r requirements.txt
+  pip install neural_compressor
+  cd examples/pytorch/image_recognition/MobileNetV2-0.35/distillation/eager
+  pip install -r requirements.txt
+  echo "======= pip list ======="
+  pip list
+  echo "Training teacher model WideResNet40-2"
+  python -u train_without_distillation.py --epochs 1 --lr 0.1 --layers 40 --widen-factor 2 --name WideResNet-40-2 --tensorboard
+  echo "Distillation"
+  python -u main.py --epochs 1 --lr 0.02 --name MobileNetV2-0.35-distillation --teacher_model runs/WideResNet-40-2/model_best.pth.tar --tensorboard --seed 9
+
+}
+
+main "$@"
